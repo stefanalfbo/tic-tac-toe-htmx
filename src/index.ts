a1acd592bc1269d14f82026eb64d9fa85fef31c0
Stefan Alfbo <stefan@alfbo.se>
2023-07-13 15:15:57 +0200

    Add htmx via CDN and add a css file

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,14 @@ import path from 'path';
 const app: Express = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, '../src/public')));
+
 app.get('/', (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, '../src/public/index.html'));
+});
+
+app.post('/move', (req: Request, res: Response) => {
+  console.log('move');
 });
 
 app.listen(port, () => {
