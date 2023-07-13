db5c66e877be173fa8a85df5df0a72a9fb099a30
Stefan Alfbo <stefan@alfbo.se>
2023-07-13 13:54:54 +0200

    Create some simple backend code and add a start script in package.json

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express, { Express, Request, Response } from 'express';
+
+
+const app: Express = express();
+const port = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Tic-Tac-Toe backend');
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
